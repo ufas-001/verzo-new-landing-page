674045d0698a689a43abbaae9c58cd183efee01d
Hufaaz <ufasabdulmutolib@gmail.com>
2025-03-24 09:34:47 +0100

    Modified the homepage

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "cdn.filestackcontent.com",
+      "verzo.fra1.cdn.digitaloceanspaces.com",
+    ],
+    disableStaticImages: true, // Disable Image Optimization
+  },
 };
 
 export default nextConfig;
