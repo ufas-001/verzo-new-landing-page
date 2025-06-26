e4a206dafacea93d9ea792f1963c0ffe9951b916
Hufaaz <ufasabdulmutolib@gmail.com>
2025-06-26 09:28:53 +0100

    Created the purchase and expense page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { FixedHeader } from "@/components/header";
 import HomePage from "./homepage/page";
 
 
 export default function Home() {
   return (
     <>
+      <FixedHeader />
       <HomePage />
     </>
   );
